554d2968379612f3c50d305e7c7fb51775f70d63
Phạm P. Nam <nampp@inspirelab.io>
2024-05-29 09:24:46 +0700

    Initial Commit

---
+++ QrCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QrCode
+//
+//  Created by P. Nam on 29/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QrCode/QrCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrCodeApp.swift
+//  QrCode
+//
+//  Created by P. Nam on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct QrCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
